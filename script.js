e6dffcb0f327f508a2bf52ee6c5772f9961e5052
voneAbides <vojtiknemecek@seznam.cz>
2021-11-08 12:54:32 +0100

    Code formated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const controls = document.querySelector(".controls");
 const container = document.querySelector(".thumbnail-container");
 const allBox = container.children;
-const containerWidth=container.offsetWidth;
+const containerWidth = container.offsetWidth;
 const margin = 30;
  var items = 0;
  var totalItems = 0;
@@ -17,7 +17,7 @@ responsive=[
 ]
 
 function load(){
-   for(let i=0; i < responsive.length; i++){
+   for(let i = 0; i < responsive.length; i++){
     if(window.innerWidth > responsive[i].breakPoint.width){
       items = responsive[i].breakPoint.item
     }
@@ -27,28 +27,28 @@ function load(){
 
 function start(){
  var totalItemsWidth=0
-for(let i=0;i<allBox.length;i++){
+for(let i = 0; i < allBox.length; i++){
    // width and margin setup of items
-  allBox[i].style.width=(containerWidth/items)-margin + "px";
-  allBox[i].style.margin=(margin/2)+ "px";
-      totalItemsWidth+=containerWidth/items;
+  allBox[i].style.width = (containerWidth/items)-margin + "px";
+  allBox[i].style.margin = (margin/2) + "px";
+      totalItemsWidth += containerWidth / items;
       totalItems++;
 }
 
 // thumbnail-container width set up
-container.style.width=totalItemsWidth + "px";
+container.style.width = totalItemsWidth + "px";
 
 // slides controls number set up
  const allSlides=Math.ceil(totalItems/items);
-   const ul=document.createElement("ul");
-      for(let i=1;i<=allSlides;i++){
+   const ul = document.createElement("ul");
+      for(let i = 1; i <= allSlides; i++){
         const li=document.createElement("li");
-             li.id=i;
-             li.innerHTML=i;
+             li.id = i;
+             li.innerHTML = i;
              li.setAttribute("onclick","controlSlides(this)");
              ul.appendChild(li);
-             if(i==1){
-              li.className="active";
+             if(i == 1){
+              li.className = "active";
              }
       }
       controls.appendChild(ul);
@@ -57,28 +57,28 @@ container.style.width=totalItemsWidth + "px";
   // when click on numbers slide to next slide
 function controlSlides(ele){
      // select controls children  'ul' element
-     const ul=controls.children;
+     const ul = controls.children;
 
      // select ul children 'li' elements;
-    const li=ul[0].children
+    const li = ul[0].children
 
 
      var active;
 
-     for(let i=0;i<li.length;i++){
-      if(li[i].className=="active"){
+     for(let i = 0;i < li.length; i++){
+      if(li[i].className == "active"){
         // find who is now active
-        active=i;
+        active = i;
         // remove active class from all 'li' elements
-        li[i].className="";
+        li[i].className = "";
       }
      }
      // add active class to current slide
-     ele.className="active";
+     ele.className = "active";
 
-     var numb=(ele.id-1)-active;
-        jumpSlideWidth=jumpSlideWidth+(containerWidth*numb);
-     container.style.marginLeft=-jumpSlideWidth + "px";
+     var numb = (ele.id-1)-active;
+        jumpSlideWidth = jumpSlideWidth + (containerWidth * numb);
+     container.style.marginLeft =- jumpSlideWidth + "px";
 }
 
 window.onload=load();
